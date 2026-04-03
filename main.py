4722176056cd6dbc3f252f4ca3727566853f0c2c
Tanishq-Sh <tanishqsharma.jn@gmail.com>
2026-04-03 20:33:45 +0100

    added Path parameter for patient_id for dynamic retrieving of patients by id

--- main.py
+++ main.py
@@ -26,3 +26,13 @@ def about():
 def view():
     data = load_data()
     return data
+
+@app.get("/patient/{patient_id}")
+def view_patient(patient_id: str):
+    # load all the patients
+    data= load_data()
+    
+    if patient_id in data:
+        return data[patient_id]
+    return {'error':'patient not found'}
+    

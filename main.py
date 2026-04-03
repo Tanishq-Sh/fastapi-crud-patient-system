66c200596de0496406678f684c547f1ea40e41e1
Tanishq-Sh <tanishqsharma.jn@gmail.com>
2026-04-03 21:22:25 +0100

    added query parameter for sorting and filtering

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Path, HTTPException 
+from fastapi import FastAPI, Path, HTTPException, Query
 import json
 
 
@@ -35,4 +35,25 @@ def view_patient(patient_id: str = Path(..., description='ID of the patient in t
     if patient_id in data:
         return data[patient_id]
     raise HTTPException(status_code=404, detail='Patient not found')
+    
+@app.get('/sort')
+def sort_patients(
+    sort_by: str = Query(..., description='Sort on the bases of height, weight or bmi'),
+    order: str = Query('asc', description='sort in asc or desc order')
+    ):
+    
+    valid_fields = ['height', 'weight', 'bmi']
+    valid_order = ['asc', 'desc']
+    
+    if sort_by not in valid_fields:
+        raise HTTPException(status_code=400, detail=f'invalid field. Select from {valid_fields}')
+    
+    if order not in ['asc', 'desc']:
+        raise HTTPException(status_code=400, detail=f'Invalid order. Select from {valid_order}')
+    
+    sort_order = True if order == 'desc' else False
+    data = load_data()
+    sorted_data = sorted(data.values(), key=lambda x: x.get(sort_by, 0), reverse=sort_order)
+    
+    return sorted_data
     

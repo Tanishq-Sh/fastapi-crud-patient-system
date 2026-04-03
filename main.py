b15d58c2513861152cb8253f04f425b25ec38ca5
Tanishq-Sh <tanishqsharma.jn@gmail.com>
2026-04-03 20:23:49 +0100

    added view endpoint to display data of all the patients

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
 from fastapi import FastAPI
+import json
 
 
 app = FastAPI()
 
+def load_data():
+    with open('patients.json', 'r') as f:
+        data = json.load(f)
+        
+    return data
+
 @app.get("/")
 def hello():
     return {
-        'message': "Hello"
+        'message': "Patient Management System API"
     }
 
 @app.get("/about")
 def about():
     return {
-        'message': "This is about page"
+        'message': "A fully functional API to manage your patient records"
     }
+    
+@app.get("/view")
+def view():
+    data = load_data()
+    return data
